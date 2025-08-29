b5de1c759a46e4709e7c7393cf8eda5e2e65ed3e
Drei08 <andrei_rheivjarth@hotmail.com>
2025-08-29 20:54:42 -0300

    feat: setup project

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import cors from 'cors';
+import { versiculos } from './mock.js';
+
+
+const app = express();
+
+app.use(cors({
+  exposedHeaders: ['x-total-count'],
+})
+);
+
+app.use(express.json());
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+app.get('/',(req, res) =>{
+  return res.status(200).json({ok: true})
+});
+
+app.get('/versiculos', (req, res) => {
+  const all = getRandomInt(versiculos.length);
+  return res.status(200).json(versiculos[all]);
+});
+
+app.listen(3000);
