67dad62489819624d4cafa1adaa0f9632d768104
Drei08 <andrei_rheivjarth@hotmail.com>
2025-09-10 07:18:11 -0300

    infra: vercel deploy/mock.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import express, { response } from "express";
 import cors from "cors";
-import { piadas } from "./mock.js";
+import { versiculos } from "./mock.js";
+
 
 const app = express();
 
 app.use(
   cors({
+    origin: 'http://localhost:5173',
     exposedHeaders: ["X-Total-Count"],
   })
 );
@@ -21,9 +23,11 @@ app.get("/", (req, res) =>{
   res.status(200).send("deu certo")
 })
 
-app.get("/piadas", (req,res) => {
-  const mock = getRandomInt(piadas.length);
-  return res.status(200).send({piada: piadas[mock]});
+app.get("/versiculos", (req,res) => {
+  const mock = getRandomInt(versiculos.length);
+  return res.status(200).send({versiculos: versiculos[mock]});
 })
 
 app.listen(3333);
+
+export default app;
--- src/mock.js
+++ src/mock.js
@@ -1,51 +1,86 @@
-export const piadas = [
+// mock.js
+
+export const versiculos = [
+
   {
-    piada: "Qual é o peixe mais inteligente? O atum!",
-    tipo: "biologia",
-    answer: null,
+    texto: "(Efésios 2:10) Se Deus plantou em você um sonho, é porque Ele já te viu vivendo a promessa."
   },
+
   {
-    piada:
-      "Por que o livro de matemática ficou triste? Porque tinha muitos problemas.",
-    tipo: "escola",
-    answer: null,
+    texto: "(Isaías 41:10) Não temas, pois Eu sou contigo. Ainda que tudo pareça difícil, Eu sou tua força."
   },
+
   {
-    piada:
-      "Qual é o cúmulo do eletricista? Ficar de mau-humor e não dar fase pra ninguém.",
-    tipo: "profissões",
-    answer: null,
+    texto: "(Salmos 46:1) Deus é o nosso refúgio e fortaleza, socorro bem presente na angústia."
   },
+
   {
-    piada:
-      "Sabe por que o lápis nunca se atrasava? Porque ele sempre estava na ponta!",
-    tipo: "escola",
-    answer: null,
+    texto: "(Filipenses 4:13) Tudo posso naquele que me fortalece. Nenhuma batalha é maior que o Deus que te sustenta."
   },
+
   {
-    piada: "O que é, o que é: Tem bico mas não canta?",
-    tipo: "adivinhação",
-    answer: "O bule.",
+    texto: "(Jeremias 29:11) Porque Eu bem sei os planos que tenho para você. E são planos de paz e não de mal."
   },
+
   {
-    piada: "O que é, o que é: Anda com os pés na cabeça?",
-    tipo: "adivinhação",
-    answer: "O piolho.",
+    texto: "(Romanos 8:28) Todas as coisas cooperam para o bem daqueles que amam a Deus. Até o que você não entende agora."
   },
+
   {
-    piada: "O que é, o que é: Tem dentes mas não morde?",
-    tipo: "adivinhação",
-    answer: "O pente.",
+    texto: "(Hebreus 11:1) A fé é a certeza do que se espera, a convicção de fatos que não se veem. Continue crendo."
   },
+
   {
-    piada: "O que é, o que é: Sempre se quebra quando se fala?",
-    tipo: "adivinhação",
-    answer: "O segredo.",
+    texto: "(2 Coríntios 5:7) Porque vivemos por fé, e não pelo que vemos. Deus está trabalhando, mesmo em silêncio."
   },
+
   {
-    piada:
-      "O que é, o que é: Não tem olhos, mas pisca. Não tem boca, mas fala?",
-    tipo: "adivinhação",
-    answer: "O farol.",
+    texto: "(Salmos 37:5) Entrega o teu caminho ao Senhor; confia nele, e o mais Ele fará. Não se preocupe, apenas creia."
+  },
+
+  {
+    texto: "(João 16:33) No mundo tereis aflições, mas tende bom ânimo. Eu venci o mundo e você também vencerá."
+  },
+
+  {
+    texto: "(Provérbios 21:23) Cuidado com os desabafos. HOJE, na sua frente são orelhas. Amanhã, nas suas costas, serão linguas."
+  },
+   
+  {
+    texto: "(Salmos 37:23) O Senhor firma os passos de um homem quando a conduta deste lhe agrada. Ainda que tropeça, não cairá, pois o Senhor o sustena pela mão."
+  },
+
+  {
+    texto: "(Genesis 28:15) Eu estarei contifo e não te abandonarei até cumprir o que te prometi. E cuidarei de ti onde quer qu vás."
+  },
+  
+  {
+    texto: "(Provérbios 19:21) Ainda que os planos mudem, Deus continua sendo bom."
+  },
+
+  {
+    texto: "(Isaías 41:10) Olhe para frente. Um guerreiro só baixa a cabeça para orar. E depois se levanta para vencer."
+  },
+  
+  {
+    texto: "(Hebreus 10:36) Tudo tem seu tempo. Tenha paciência. Deus sabe o que é melhor para você."
+  },
+  
+  {
+    texto: "(Jeremeis 29:11) Até os sonhos do teu coração, fui Eu que coloquei. Achas mesmo que Eu não te darei recursos para vivê-los?"
+  },
+  
+  {
+    texto: "(Provérbios 13:3) Nunca conte às pessoas, mais do que elas precisam saber. Pois baú aberto, não protege tesouro!"
+  },
+
+  
+  {
+    texto: "(Josué 1:9) Seja forte e corajoso! Não se apavore, nem desanime, pois o Senhor, o seu Deus, estará com você por onde for."
+  },
+  
+  {
+    texto: "(Jeremeias 1:10) Deus às vezes precisa descontruir algumas coisas na nossa vida para construir algo muito maior e melhor."
   },
 ];
+
